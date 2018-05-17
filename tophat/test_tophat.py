ab738cf9fa40189b6b6216a90eb683890dd3e0ff
Josh Mitchell <yoshanuikabundi@gmail.com>
2018-05-17 14:44:37 +1000

    tophat now works with general #commands and the [ system ] directive

--- tophat/test_tophat.py
+++ tophat/test_tophat.py
@@ -33,14 +33,9 @@ def test_Topology_molecules():
         "water                          12843"
     ])
     assert str(top.molecules) == compstr
-    assert top.unparsed == [
-        "[ system ]",
-        "; this is for gmx btw",
-        "Test system - microbilayer!",
-    ]
     compstr = '\n'.join([
         "[ system ]",
-        "; this is for gmx btw",
+        "; name",
         "Test system - microbilayer!",
         "",
         "[ molecules ]",
@@ -59,7 +54,7 @@ def test_Topology_molecules():
     top.molecules *= 10
     compstr = '\n'.join([
         "[ system ]",
-        "; this is for gmx btw",
+        "; name",
         "Test system - microbilayer!",
         "",
         "[ molecules ]",
@@ -76,11 +71,12 @@ def test_Topology_molecules():
     assert str(top) == compstr
 
 
-def test_Topology_includes():
+def test_Topology_hashcommands():
     strlist = [
+        '#include "./martini_v2.2refP.itp" ; comment',
+        "#define RUBBERBANDS",
         "#include './martini_protein.itp'",
         "",
-        '#include "./martini_v2.2refP.itp" ; comment',
         "#include martini_v2.0_ions.itp",
         "  [ system ]   ",
         "; this is for gmx btw",
@@ -100,17 +96,23 @@ def test_Topology_includes():
     top = Topology()
     top.read(strlist)
 
-    top.includes = ["'./martini_protein.itp'", '"./martini_v2.2refP.itp"', "martini_v2.0_ions.itp"]
-    top.includes.append('martini_v2.0_sugars.itp')
+    assert top.hashcommands == [
+        '#include "./martini_v2.2refP.itp"',
+        "#define RUBBERBANDS",
+        "#include './martini_protein.itp'",
+        "#include martini_v2.0_ions.itp"
+    ]
+    top.hashcommands.append('#include martini_v2.0_sugars.itp')
 
     compstr = '\n'.join([
-        "#include './martini_protein.itp'",
         '#include "./martini_v2.2refP.itp"',
+        "#define RUBBERBANDS",
+        "#include './martini_protein.itp'",
         "#include martini_v2.0_ions.itp",
         "#include martini_v2.0_sugars.itp",
         "",
         "[ system ]",
-        "; this is for gmx btw",
+        "; name",
         "Test system - microbilayer!",
         "",
         "[ molecules ]",
@@ -125,4 +127,80 @@ def test_Topology_includes():
         ""
     ])
     assert str(top) == compstr
+
+
+def test_Topology_system():
+    strlist = [
+        '#include "./martini_v2.2refP.itp" ; comment',
+        "#define RUBBERBANDS",
+        "#include './martini_protein.itp'",
+        "",
+        "#include martini_v2.0_ions.itp",
+        "[ molecules ]  ; Molecule section time!!!",
+        ";name ;number",
+        "protein   1; the protein itself",
+        "protein_with_a_very_long_name   1",
+        "lipid_a 2",
+        " lipid_b  5",
+        ";lipid_c     4\n",
+        "lipid_a      2",
+        "lipid_b\t7",
+        "",
+        "water    12843;solvent!?!"
+    ]
+    top = Topology()
+    top.read(strlist)
+
+    with pytest.raises(ValueError):
+        str(top)
+
+    top.name = "Test system - microbilayer!"
+
+    compstr = '\n'.join([
+        '#include "./martini_v2.2refP.itp"',
+        "#define RUBBERBANDS",
+        "#include './martini_protein.itp'",
+        "#include martini_v2.0_ions.itp",
+        "",
+        "[ system ]",
+        "; name",
+        "Test system - microbilayer!",
+        "",
+        "[ molecules ]",
+        ";name                          count",
+        "protein                            1",
+        "protein_with_a_very_long_name      1",
+        "lipid_a                            2",
+        "lipid_b                            5",
+        "lipid_a                            2",
+        "lipid_b                            7",
+        "water                          12843",
+        ""
+    ])
+    assert str(top) == compstr
+
+    strlist = [
+        '#include "./martini_v2.2refP.itp"',
+        "#define RUBBERBANDS",
+        "#include './martini_protein.itp'",
+        "",
+        "#include martini_v2.0_ions.itp",
+        "[ system ]",
+        "; name",
+        "Test system - microbilayer! - but inconsistent name",
+        "",
+        "[ molecules ]  ; Molecule section time!!!",
+        ";name ;number",
+        "protein   1; the protein itself",
+        "protein_with_a_very_long_name   1",
+        "lipid_a 2",
+        " lipid_b  5",
+        ";lipid_c     4\n",
+        "lipid_a      2",
+        "lipid_b\t7",
+        "",
+        "water    12843;solvent!?!"
+    ]
+    with pytest.raises(ValueError):
+        top.read(strlist)
 
--- tophat/tophat.py
+++ tophat/tophat.py
@@ -9,7 +9,8 @@ class Topology:
     def __init__(self, fname=None):
         self.unparsed = []
         self.molecules = MoleculesSection()
-        self.includes = []
+        self.hashcommands = []
+        self.name = ''
         if fname:
             with open(fname) as f:
                 self.read(f)
@@ -17,12 +18,20 @@ class Topology:
             self.molecules = MoleculesSection()
 
     def __str__(self):
-        out = [f"#include {i}" for i in self.includes]
+        if not self.name:
+            raise ValueError("GROMACS topology must have a name!")
+
+        out  = list(self.hashcommands)
         out += ['']
         out += self.unparsed
         out += ['']
+        out += ['[ system ]']
+        out += ['; name']
+        out += [self.name]
+        out += ['']
         out += [str(self.molecules)]
         out += [''] # I am very proud of this line
+        
         out = [i for n,i in enumerate(out) if i != "" or out[n-1] != ""] 
         return '\n'.join(out)
 
@@ -37,7 +46,8 @@ class Topology:
 
             # We need to set up our directives now
             casedict = {
-                    'molecules': self._read_molecules
+                    'molecules': self._read_molecules,
+                    'system': self._read_system
                 }
 
             # Is this line a directive?
@@ -50,7 +60,17 @@ class Topology:
             readerfunc = casedict.get(current_directive, self._read_default)
             readerfunc(line, comments)
 
+    def _read_system(self, line, _):
+        if line[:1] == "#":
+            raise ValueError("Hashcommand after [ system ] directive not supported")
+        if self.name and line and self.name != line:
+            raise ValueError(f'System name defined ambiguously: "{self.name}" and "{line}"')
+        elif line:
+            self.name = line
+
     def _read_molecules(self, line, _):
+        if line[:1] == "#":
+            raise ValueError("Hashcommand after [ system ] directive not supported")
         if line:
             name, count = line.split()
             name = MoleculeType(name)
@@ -58,8 +78,8 @@ class Topology:
             self.molecules.append(name, count)
 
     def _read_default(self, line, comments):
-        if line[:9] == "#include ":
-            self.includes.append(line[9:])
+        if line[:1] == "#":
+            self.hashcommands.append(line)
             return 
         elif comments:
             self.unparsed.append(';'.join([line] + comments))
