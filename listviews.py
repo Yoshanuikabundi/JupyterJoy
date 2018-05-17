190fd7df22ef8f3d0458fa7649049412420bd3cb
Josh Mitchell <yoshanuikabundi@gmail.com>
2018-05-17 19:27:31 +1000

    Improved listviews inheritance and added mutations

--- listviews.py
+++ listviews.py
@@ -1,5 +1,6 @@
 from collections.abc import MutableSequence
 from abc import abstractmethod
+from contextlib import suppress
 import re
 
 class ListView(MutableSequence):
@@ -8,16 +9,16 @@ class ListView(MutableSequence):
         self.alist = alist
 
     @abstractmethod
-    def __filtered__(self):
-        """Filter the desired elements in alist
+    def map_view(self):
+        """Map the view to the desired elements in alist
 
-        __filtered__ must return a sequence of pairs of original alist
+        map_view must return a sequence of pairs of original alist
         indices and their corresponding values."""
-        pass
+        return enumerate(self.alist)
 
     @property
     def _filtered(self):
-        return list(self.__filtered__())
+        return list(self.map_view())
 
     def __getitem__(self, key):
         return self._filtered[key][1]
@@ -45,22 +46,73 @@ class ListView(MutableSequence):
 
 class ValidateListView(ListView):
     @abstractmethod
-    def __filter__(self, value):
+    def validate(self, value):
         """Only values that return true here are included in the view"""
-        pass
+        return True
 
     def __setitem__(self, key, value):
-        if not self.__filter__(value):
+        if not self.validate(value):
             raise ValueError(f'{value} is not a valid element of {self}')
         super().__setitem__(key, value)
         
     def insert(self, key, value):
-        if not self.__filter__(value):
+        if not self.validate(value):
             raise ValueError(f'{value} is not a valid element of {self}')
         super().insert(key, value)
 
-    def __filtered__(self):
-        return ((i,v) for i,v in enumerate(self.alist) if self.__filter__(v))
+    def map_view(self):
+        return ((i,v) for i,v in super().map_view() if self.validate(v))
+
+class MutateListView(ListView):
+    @abstractmethod
+    def mutate(self, value):
+        """Values in the view are mutated by this method"""
+        return value
+
+    @abstractmethod
+    def demutate(self, value):
+        """Values modified in the view are mutated by this method"""
+        return value
+
+    def __setitem__(self, key, value):
+        super().__setitem__(key, self.demutate(value))
+        
+    def insert(self, key, value):
+        super().insert(key, self.demutate(value))
+
+    def map_view(self):
+        return ((i,self.mutate(v)) for i,v in super().map_view())
+
+
+class SliceListView(ListView):
+    def __init__(self, alist, *args, **kwargs):
+        if 'stop' in kwargs:
+            start = kwargs.pop('start', 0)
+            stop  = kwargs.pop('stop')
+            step  = kwargs.pop('step', 1)
+        elif len(args) >= 3:
+            start, stop, step, *args = args
+        elif len(args) == 2:
+            start, stop = args
+            step, args  = 1, []
+        elif len(args) == 1:
+            stop,  = args
+            start, step, args  = 0, 1, []
+        else:
+            raise ValueError("Need more arguments!")
+
+        super().__init__(alist, *args, **kwargs)
+
+        self.slice = slice(start, stop, step)
+
+    def map_view(self):
+        indices = self.slice.indices(len(self.alist))
+        values = self.alist.__getitem__(self.slice)
+        return zip(range(*indices), values)
+
+    def __repr__(self):
+        return f'SliceListView({self.alist}, {self.slice.start}, {self.slice.stop}, {self.slice.step})'
+
 
 class REListView(ValidateListView):
     def __init__(self, alist, regex, *args, **kwargs):
@@ -69,8 +121,8 @@ class REListView(ValidateListView):
             regex = re.compile(regex)
         self.regex = regex
 
-    def __filter__(self, value):
-        return self.regex.match(value)
+    def validate(self, value):
+        return self.regex.match(value) and super().validate(value)
 
     def __repr__(self):
         return f'REListView({self.alist}, {self.regex})'
--- test_listviews.py
+++ test_listviews.py
@@ -3,8 +3,8 @@ from listviews import *
 import re
 
 class TrivialListView(ListView):
-    def __filtered__(self):
-        return enumerate(self.alist)
+    def map_view(self):
+        return super().map_view()
 
 def test_ListView_append_insert():
     a = [1,2,3,4,5,6]
@@ -74,7 +74,6 @@ def test_ListView_get_set_del():
     assert a == b == aview
     assert len(a) == len(b) == len(aview)
 
-    print(a)
     a[4] = "hi"
     b[4] = "hi"
     assert a == b == aview
@@ -85,20 +84,39 @@ def test_ListView_get_set_del():
     assert a == b == aview
     assert len(a) == len(b) == len(aview)
 
-def test_ListView_bigwun():
-    a = list(range(1000))
-    b = list(range(1000))
-    aview = TrivialListView(a)
+    # assert a[3:4] == b[3:4] == aview[3:4]
 
-    del aview[500]
-    del b[500]
+    # aview[3:6] = ['a', 'b', 'c']
+    # b[3:6] = ['a', 'b', 'c']
+    # assert a == b == aview
+    # assert len(a) == len(b) == len(aview)
 
-    assert a == b == aview
+def test_SliceListView_bigwun():
+    a = list(range(1_000_000))
+    b = list(range(1_000_000))
+    start, stop, step = None, None, 100_000
+    aview = SliceListView(a, start, stop, step)
+
+    idx = 5
+    del aview[idx]
+    del b[idx*step]
+
+    assert a == b 
+    assert a[::step] == b[::step] == aview
+    assert len(a[::step]) == len(b[::step]) == len(aview)
+
+    idx = 7
+    del a[idx*step]
+    del b[idx*step]
+
+    assert a == b 
+    assert a[::step] == b[::step] == aview
+    assert len(a[::step]) == len(b[::step]) == len(aview)
 
 
 class TrivialValidateListView(ValidateListView):
-    def __filter__(self, value):
-        return True
+    def validate(self, value):
+        return super().validate(value)
 
 def test_ValidateListView_append_insert():
     a = [1,2,3,4,5,6]
@@ -208,5 +226,131 @@ def test_REListView():
     assert l == ["hello","gday","yo","yodelehihoo","sup","heya","howzit"]
     assert hyview == ["hello","yo","yodelehihoo","heya","howzit"]
     assert gyview == ["gday","yo","yodelehihoo"]
+    assert l != hyview != gyview
+
+class DoubleListView(MutateListView):
+    def mutate(self, value):
+        return super().mutate(value * 2)
+    def demutate(self, value):
+        return super().mutate(value / 2)
+
+def test_MutateListView_append_insert():
+    a = [1,2,3,4,5,6]
+    b = [2,4,6,8,10,12]
+    aview = DoubleListView(a)
+
+    assert b == aview
+    assert len(a) == len(b) == len(aview)
+
+    a.append(7)
+    b.append(14)
+    assert b == aview
+    assert len(a) == len(b) == len(aview)
+
+    aview.append(8)
+    b.append(8)
+    assert b == aview
+    assert len(a) == len(b) == len(aview)
+
+    aview.insert(5, 9)
+    b.insert(5, 9)
+    assert b == aview
+    assert len(a) == len(b) == len(aview)
+
+    a.insert(5, 3.5)
+    b.insert(5, 7)
+    assert b == aview
+    assert len(a) == len(b) == len(aview)
+
+    aview.insert(-1, 50)
+    b.insert(-1, 50)
+    assert b == aview
+    assert len(a) == len(b) == len(aview)
+
+    a.insert(-1, 80)
+    b.insert(-1, 160)
+    assert b == aview
+    assert len(a) == len(b) == len(aview)
+
+def test_MutateListView_get_set_del():
+    a = [1,2,3,4,5,6,7,8,9,10]
+    b = [2,4,6,8,10,12,14,16,18,20]
+    aview = DoubleListView(a)
+
+    assert b == aview
+    assert len(a) == len(b) == len(aview)
+
+    assert a[3] == 4
+    assert aview[3] == 8
+    assert b[3] == 8
+    assert b == aview
+    assert len(a) == len(b) == len(aview)
+
+    assert a[-1] == 10
+    assert aview[-1] == 20
+    assert b[-1] == 20
+    assert b == aview
+    assert len(a) == len(b) == len(aview)
+
+    del a[3]
+    del b[3]
+    assert b == aview
+    assert len(a) == len(b) == len(aview)
+
+    del aview[4]
+    del b[4]
+    assert b == aview
+    assert len(a) == len(b) == len(aview)
+
+    print(a)
+    a[4] = 132
+    b[4] = 264
+    assert b == aview
+    assert len(a) == len(b) == len(aview)
+
+    aview[-1] = 196
+    b[-1] = 196
+    assert b == aview
+    assert len(a) == len(b) == len(aview)
+
+class ExcitedREListView(REListView, MutateListView):
+    def mutate(self, value):
+        return super().mutate(value + '!')
+    def demutate(self, value):
+        if value[-1] != '!':
+            raise ValueError("View must take values ending in '!'")
+        return super().mutate(value[:-1])
+
+def test_ListView_multiple_inheritance():
+    l = ["hello","gday","yo","sup","howzit"]
+    re_pattern = re.compile(r'[hy]')
+    hyview = ExcitedREListView(l, re_pattern)
+    gyview = ExcitedREListView(l, r'[gy]')
+
+    assert hyview == ["hello!","yo!","howzit!"]
+    assert gyview == ["gday!","yo!"]
+
+    with pytest.raises(ValueError):
+        gyview.append("hi")
+    with pytest.raises(ValueError):
+        gyview[1] = ("hi")
+    with pytest.raises(ValueError):
+        gyview.append("yodelehihoo")
+    gyview.append("yodelehihoo!")
+    assert l == ["hello","gday","yo","yodelehihoo","sup","howzit"]
+    assert hyview == ["hello!","yo!","yodelehihoo!","howzit!"]
+    assert gyview == ["gday!","yo!","yodelehihoo!"]
+    assert l != hyview != gyview
+
+    l.insert(-1, "hi")
+    assert l == ["hello","gday","yo","yodelehihoo","sup","hi","howzit"]
+    assert hyview == ["hello!","yo!","yodelehihoo!","hi!","howzit!"]
+    assert gyview == ["gday!","yo!","yodelehihoo!"]
+    assert l != hyview != gyview
+
+    hyview[3] = "heya!"
+    assert l == ["hello","gday","yo","yodelehihoo","sup","heya","howzit"]
+    assert hyview == ["hello!","yo!","yodelehihoo!","heya!","howzit!"]
+    assert gyview == ["gday!","yo!","yodelehihoo!"]
     assert l != hyview != gyview
 
