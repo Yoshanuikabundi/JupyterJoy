04d7bd77b5ee27c8cd6e392fa7811a2f53f49459
Josh Mitchell <yoshanuikabundi@gmail.com>
2018-05-21 16:15:25 +1000

    listviews now support slicing!!! and are more thoroughly tested

--- listviews.py
+++ listviews.py
@@ -1,48 +1,193 @@
 from collections.abc import MutableSequence
-from abc import abstractmethod
+from abc import abstractmethod, ABCMeta
 from contextlib import suppress
 import re
 
-class ListView(MutableSequence):
+class ListViewMeta(ABCMeta):
+    def __init__(cls, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        cls._SubView = None
+
+    @property
+    def SubView(cls):
+        if cls._SubView is None:
+            cls._SubView = type(
+                f"{cls.__name__}.SubView", 
+                (cls, SubView),
+                {}
+            )
+
+        return cls._SubView
+
+class ListView(MutableSequence, metaclass=ListViewMeta):
     def __init__(self, alist, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.alist = alist
 
-    @abstractmethod
-    def map_view(self):
-        """Map the view to the desired elements in alist
-
-        map_view must return a sequence of pairs of original alist
-        indices and their corresponding values."""
-        return enumerate(self.alist)
+    @property
+    def SubView(self):
+        return self.__class__.SubView
 
     @property
-    def _filtered(self):
-        return list(self.map_view())
+    @abstractmethod
+    def indices(self):
+        """Return a Sequence (eg, list, tuple) of indices in the view"""
+        return tuple(range(len(self.alist)))
+
+    def create_subview(self, idx, *args, **kwargs):
+        """Create a view to return for slicing
+
+        This is a bit tricky. ListView classes automatically create a
+        class for their own subviews called cls.SubView. These are
+        returned when a slice is requested of the ListView. cls.SubView
+        is a subclass of the ListView class itself as well as 
+        ExplicitListView so it can be given arbitrary indices. What this
+        means is that when a ListView creates a subview , it needs to be 
+        able to provide all the arguments it would need to create a new 
+        instance of its own class. This method lets it do that.
+
+        When a subclass of ListView changes the argument signature of its
+        __init__method, it must also redefine this method.
+        For an example, see REListView."""
+        return self.SubView(self.alist, indices=idx, *args, **kwargs)
 
     def __getitem__(self, key):
-        return self._filtered[key][1]
+        idx = self.indices[key]
+        try:
+            return self.alist[idx]
+        except TypeError:
+            return self.create_subview(idx)
 
     def __setitem__(self, key, value):
-        idx = self._filtered[key][0]
-        self.alist[idx] = value
+        # print(f"__setitem__({repr(key)}, {repr(value)})")
+        idx = self.indices[key]
+        try:
+            self.alist[idx] = value
+            return
+        except TypeError:
+            view = self.create_subview(idx)
+            value = list(value)
+
+        assert len(idx) == len(view)
+        assert isinstance(key, slice)
+
+        start, stop, step = key.start, key.stop, key.step
+        if len(value) == len(idx):
+            for i,v in enumerate(value):
+                view[i] = v
+        elif step not in (1, None):
+            raise ValueError(f"attempt to assign sequence of size {len(value)} to extended slice of size {len(idx)}")
+        else:
+            # Standard slice
+            if start is None:
+                start = 0
+            if stop is None:
+                stop = len(self)
+
+            # Express indices positively between 0 and len(self)
+            if start < -len(self):
+                start = 0
+            elif start < 0:
+                start += len(self)
+            if stop < -len(self):
+                stop = 0
+            if stop < 0:
+                stop += len(self)
+            start = min(start, len(self))
+            stop = min(stop, len(self))
+
+            assert 0 <= start <= len(self)
+            assert 0 <= stop <= len(self)
+
+            del self[key]
+            for v in reversed(value):
+                if len(self) == 0:
+                    if start < 0:
+                        idx = -1
+                    else:
+                        idx = 0
+                elif start >= len(self):
+                    idx = self.indices[-1] + 1
+                else:
+                    idx = self.indices[start]
+
+                self.alist.insert(idx, v)
 
     def __delitem__(self, key):
-        idx = self._filtered[key][0]
-        del self.alist[idx]
+        idx = self.indices[key]
+        if isinstance(idx, int):
+            del self.alist[idx]
+        else:
+            for n,i in enumerate(sorted(set(idx))):
+                del self.alist[i-n]
+        return idx
 
     def __len__(self):
-        return len(self._filtered)
+        return len(self.indices)
 
     def __eq__(self, other):
         return list(self) == other
 
     def insert(self, key, value):
-        if key == len(self):
-            idx = self._filtered[-1][0] + 1
+        if len(self) == 0:
+            if key < 0:
+                idx = -1
+            else:
+                idx = 0
+        elif key >= len(self):
+            idx = self.indices[-1] + 1
         else:
-            idx = self._filtered[key][0]
+            idx = self.indices[key]
+
         self.alist.insert(idx, value)
+        return idx
+
+    def __repr__(self):
+        clsname = self.__class__.__name__
+        values = type(self.indices)(self)
+        return f"<{clsname} object bearing {values}>"
+
+class ExplicitListView(ListView):
+    def __init__(self, alist, indices, *args, **kwargs):
+        super().__init__(alist, *args, **kwargs)
+        self._indices = list(indices)
+
+    @property
+    def indices(self):
+        return self._indices
+
+    @indices.setter
+    def indices(self, other):
+        self._indices = list(other)
+
+    @property
+    def SubView(self):
+        return self.__class__
+
+    def __getitem__(self, key):
+        if isinstance(key, slice):
+            raise ValueError("ExplicitListView does not support slicing (yet?)")
+        return super().__getitem__(key)
+
+    def __setitem__(self, key, value):
+        if isinstance(key, slice):
+            raise ValueError("ExplicitListView does not support slicing (yet?)")
+        super().__setitem__(key, value)
+
+    def __delitem__(self, key):
+        if isinstance(key, slice):
+            raise ValueError("ExplicitListView does not support slicing (yet?)")
+        super().__delitem__(key)
+
+    def insert(self, key, value):
+        if isinstance(key, slice):
+            raise ValueError("ExplicitListView does not support slicing (yet?)")
+        super().insert(key, value)
+
+
+
+class SubView(ExplicitListView):
+    pass
 
 class ValidateListView(ListView):
     @abstractmethod
@@ -50,18 +195,27 @@ class ValidateListView(ListView):
         """Only values that return true here are included in the view"""
         return True
 
-    def __setitem__(self, key, value):
+    def _check_validation(self, value):
         if not self.validate(value):
             raise ValueError(f'{value} is not a valid element of {self}')
+
+    def __setitem__(self, key, value):
+        if isinstance(key, slice):
+            for v in value:
+                self._check_validation(v)
+        else:
+            self._check_validation(value)
         super().__setitem__(key, value)
         
     def insert(self, key, value):
-        if not self.validate(value):
-            raise ValueError(f'{value} is not a valid element of {self}')
+        self._check_validation(value)
         super().insert(key, value)
 
-    def map_view(self):
-        return ((i,v) for i,v in super().map_view() if self.validate(v))
+    @property
+    def indices(self):
+        validate = self.validate
+        alist = self.alist
+        return tuple(i for i in super().indices if validate(alist[i]))
 
 class MutateListView(ListView):
     @abstractmethod
@@ -74,14 +228,27 @@ class MutateListView(ListView):
         """Values modified in the view are mutated by this method"""
         return value
 
+    def __getitem__(self, key):
+        out = super().__getitem__(key)
+        if isinstance(out, self.SubView):
+            # cls.SubView has the same mutate method, so out is already done
+            return out
+        else:
+            return self.mutate(out)
+
     def __setitem__(self, key, value):
-        super().__setitem__(key, self.demutate(value))
-        
+        if isinstance(key, slice):
+            values = (self.demutate(v) for v in value)
+            super().__setitem__(key, values)
+        else:
+            super().__setitem__(key, self.demutate(value))
+
     def insert(self, key, value):
         super().insert(key, self.demutate(value))
 
-    def map_view(self):
-        return ((i,self.mutate(v)) for i,v in super().map_view())
+    @property
+    def indices(self):
+        return super().indices
 
 
 class SliceListView(ListView):
@@ -105,13 +272,10 @@ class SliceListView(ListView):
 
         self.slice = slice(start, stop, step)
 
-    def map_view(self):
-        indices = self.slice.indices(len(self.alist))
-        values = self.alist.__getitem__(self.slice)
-        return zip(range(*indices), values)
-
-    def __repr__(self):
-        return f'SliceListView({self.alist}, {self.slice.start}, {self.slice.stop}, {self.slice.step})'
+    @property
+    def indices(self):
+        start, stop, step = self.slice.indices(len(self.alist))
+        return range(start, stop, step)
 
 
 class REListView(ValidateListView):
@@ -124,5 +288,10 @@ class REListView(ValidateListView):
     def validate(self, value):
         return self.regex.match(value) and super().validate(value)
 
-    def __repr__(self):
-        return f'REListView({self.alist}, {self.regex})'
+    def create_subview(self, *args, **kwargs):
+        """Create a view to return for slicing
+
+        I added a new required argument 'regex' to __init__, so I need 
+        to show REListView how to create a subview of itself. This is
+        the method in which that happens."""
+        return super().create_subview(regex=self.regex, *args, **kwargs)
--- test_listviews.py
+++ test_listviews.py
@@ -3,8 +3,9 @@ from listviews import *
 import re
 
 class TrivialListView(ListView):
-    def map_view(self):
-        return super().map_view()
+    @property
+    def indices(self):
+        return super().indices
 
 def test_ListView_append_insert():
     a = [1,2,3,4,5,6]
@@ -39,22 +40,65 @@ def test_ListView_append_insert():
     assert a == b == aview
     assert len(a) == len(b) == len(aview)
 
+    aview.insert(0, 68)
+    b.insert(0, 68)
+    assert a == b == aview
+    assert len(a) == len(b) == len(aview)
+
     a.insert(-1, '80')
     b.insert(-1, '80')
     assert a == b == aview
     assert len(a) == len(b) == len(aview)
 
+    a.insert(-8, 38)
+    b.insert(-8, 38)
+    assert a == b == aview
+    assert len(a) == len(b) == len(aview)
+
+    a = []
+    b = []
+    aview = TrivialListView(a)
+    aview.append(3)
+    b.append(3)
+    assert a == b == aview
+    assert len(a) == len(b) == len(aview)
+
+    a = []
+    b = []
+    aview = TrivialListView(a)
+    aview.insert(0,3)
+    b.insert(0,3)
+    assert a == b == aview
+    assert len(a) == len(b) == len(aview)
+
+    a = []
+    b = []
+    aview = TrivialListView(a)
+    aview.insert(1,3)
+    b.insert(1,3)
+    assert a == b == aview
+    assert len(a) == len(b) == len(aview)
+
+    a = []
+    b = []
+    aview = TrivialListView(a)
+    aview.insert(-1,3)
+    b.insert(-1,3)
+    assert a == b == aview
+    assert len(a) == len(b) == len(aview)
+
+
 def test_ListView_get_set_del():
-    a = [1,2,3,4,5,6,7,8,9,10]
-    b = [1,2,3,4,5,6,7,8,9,10]
+    a = [0,1,2,3,4,5,6,7,8,9,10]
+    b = [0,1,2,3,4,5,6,7,8,9,10]
     aview = TrivialListView(a)
 
     assert a == b == aview
     assert len(a) == len(b) == len(aview)
 
-    assert a[3] == 4
-    assert aview[3] == 4
-    assert b[3] == 4
+    assert a[3] == 3
+    assert aview[3] == 3
+    assert b[3] == 3
     assert a == b == aview
     assert len(a) == len(b) == len(aview)
 
@@ -84,16 +128,121 @@ def test_ListView_get_set_del():
     assert a == b == aview
     assert len(a) == len(b) == len(aview)
 
-    # assert a[3:4] == b[3:4] == aview[3:4]
+def slices():
+    numbers = [0, 1, 2, 3, 9, 10, 11, 12, 20]
+    for start in numbers:
+        for stop in numbers:
+            for step in numbers:
+                combos = [
+                    ( start,  stop,  step),
+                    ( start,  stop, -step),
+                    ( start,  stop,  None),
+                    ( start, -stop,  step),
+                    ( start, -stop, -step),
+                    ( start, -stop,  None),
+                    ( start,  None,  step),
+                    ( start,  None, -step),
+                    ( start,  None,  None),
+                    (-start,  stop,  step),
+                    (-start,  stop, -step),
+                    (-start,  stop,  None),
+                    (-start, -stop,  step),
+                    (-start, -stop, -step),
+                    (-start, -stop,  None),
+                    (-start,  None,  step),
+                    (-start,  None, -step),
+                    (-start,  None,  None),
+                    (  None,  stop,  step),
+                    (  None,  stop, -step),
+                    (  None,  stop,  None),
+                    (  None, -stop,  step),
+                    (  None, -stop, -step),
+                    (  None, -stop,  None),
+                    (  None,  None,  step),
+                    (  None,  None, -step),
+                    (  None,  None,  None)
+                ]
+                yield from combos
 
-    # aview[3:6] = ['a', 'b', 'c']
-    # b[3:6] = ['a', 'b', 'c']
-    # assert a == b == aview
-    # assert len(a) == len(b) == len(aview)
+def test_ListView_slicing():
+    a = [0,1,2,3,4,5,6,7,8,9,10,11]
+    b = [0,1,2,3,4,5,6,7,8,9,10,11]
+    aview = TrivialListView(a)
+    assert a == b == aview
+    assert len(a) == len(b) == len(aview)
+
+    for start, stop, step in slices():
+        a = [0,1,2,3,4,5,6,7,8,9,10,11]
+        b = [0,1,2,3,4,5,6,7,8,9,10,11]
+        aview = TrivialListView(a)
+        try:
+            bsl = b[start:stop:step]
+        except Exception as e:
+            with pytest.raises(type(e)):
+                avsl = aview[start:stop:step]
+        else:
+            try:
+                asl = a[start:stop:step]
+                avsl = aview[start:stop:step]
+                assert asl == bsl == avsl
+            except AssertionError as e:
+                print(f"seq[{start}:{stop}:{step}]")
+                raise e
+
+def test_ListView_delslicing():
+    for start, stop, step in slices():
+        a = [0,1,2,3,4,5,6,7,8,9,10,11]
+        b = [0,1,2,3,4,5,6,7,8,9,10,11]
+        aview = TrivialListView(a)
+        try:
+            del b[start:stop:step]
+        except Exception as e:
+            with pytest.raises(type(e)):
+                del aview[start:stop:step]
+        else:
+            try:
+                del aview[start:stop:step]
+                assert b == a
+                assert a == b == aview
+                assert len(a) == len(b) == len(aview)
+            except Exception as e:
+                print(f"del seq[{start}:{stop}:{step}]")
+                raise e
+
+def test_ListView_setslicing():
+    vals = [
+        '',
+        'a',
+        'ab',
+        'abc',
+        'abcd',
+        'abcde',
+        'abcdefghijklmnopqrstuvwxyz'
+    ]
+    for start, stop, step in slices():
+        for value in vals:
+            a = [0,1,2,3,4,5,6,7,8,9,10,11]
+            b = [0,1,2,3,4,5,6,7,8,9,10,11]
+            aview = TrivialListView(a)
+            try:
+                b[start:stop:step] = value
+            except Exception as e:
+                with pytest.raises(type(e)):
+                    aview[start:stop:step] = value
+            else:
+                try:
+                    aview[start:stop:step] = value
+                    assert b == a
+                    assert a == b == aview
+                    assert len(a) == len(b) == len(aview)
+                except Exception as e:
+                    print(f"seq[{start}:{stop}:{step}] = {repr(value)}")
+                    print(f"len(idx): {len(aview.indices[start:stop:step])}")
+                    raise e
 
 def test_SliceListView_bigwun():
-    a = list(range(1_000_000))
-    b = list(range(1_000_000))
+    a = list(range(1_100_000))
+    b = list(range(1_100_000))
     start, stop, step = None, None, 100_000
     aview = SliceListView(a, start, stop, step)
 
@@ -186,7 +335,6 @@ def test_ValidateListView_get_set_del():
     assert a == b == aview
     assert len(a) == len(b) == len(aview)
 
-    print(a)
     a[4] = "hi"
     b[4] = "hi"
     assert a == b == aview
@@ -196,6 +344,89 @@ def test_ValidateListView_get_set_del():
     b[-1] = "world"
     assert a == b == aview
     assert len(a) == len(b) == len(aview)
+
+class OddValidateListView(ValidateListView):
+    def validate(self, value):
+        return super().validate(value) and value % 2 
+
+def test_OddValidateListView_slicing():
+    a = [0,1,2,3,4,5,6,7,8,9,10,11]
+    b = [1,3,5,7,9,11]
+    aview = OddValidateListView(a)
+    assert b == aview
+    assert len(b) == len(aview)
+
+    for start, stop, step in slices():
+        a = [0,1,2,3,4,5,6,7,8,9,10,11]
+        b = [1,3,5,7,9,11]
+        aview = OddValidateListView(a)
+        try:
+            bsl = b[start:stop:step]
+        except Exception as e:
+            with pytest.raises(type(e)):
+                avsl = aview[start:stop:step]
+        else:
+            try:
+                avsl = aview[start:stop:step]
+                assert bsl == avsl
+            except AssertionError as e:
+                print(f"seq[{start}:{stop}:{step}]")
+                raise e
+
+def test_OddValidateListView_delslicing():
+    for start, stop, step in slices():
+        a = [0,1,2,3,4,5,6,7,8,9,10,11]
+        b = [1,3,5,7,9,11]
+        aview = OddValidateListView(a)
+        try:
+            del b[start:stop:step]
+        except Exception as e:
+            with pytest.raises(type(e)):
+                del aview[start:stop:step]
+        else:
+            try:
+                del aview[start:stop:step]
+                assert b == aview
+                assert len(b) == len(aview)
+            except Exception as e:
+                print("a:", a)
+                print("aview.indices:", aview.indices)
+                print(f"del seq[{start}:{stop}:{step}]")
+                raise e
+
+def test_OddValidateListView_setslicing():
+    vals = [
+        [],
+        [901],
+        [901,903],
+        [901,903,905],
+        [901,903,905,907],
+        [901,903,905,907],
+        [901,903,905,907,909,911,913,915,917,919],
+    ]
+    for start, stop, step in slices():
+        for value in vals:
+            a = [0,1,2,3,4,5,6,7,8,9,10,11]
+            b = [1,3,5,7,9,11]
+            aview = OddValidateListView(a)
+            print(a, aview)
+            try:
+                b[start:stop:step] = value
+            except Exception as e:
+                with pytest.raises(type(e)):
+                    aview[start:stop:step] = value
+            else:
+                try:
+                    aview[start:stop:step] = value
+                    assert b == aview
+                    assert len(b) == len(aview)
+                    for i in a:
+                        if i % 2:
+                            assert b.pop(0) == i
+                except Exception as e:
+                    print(f"seq[{start}:{stop}:{step}] = {repr(value)}")
+                    print(f"len(idx): {len(aview.indices[start:stop:step])}")
+                    raise e
 
 def test_REListView():
     l = ["hello","gday","yo","sup","howzit"]
@@ -302,7 +533,6 @@ def test_MutateListView_get_set_del():
     assert b == aview
     assert len(a) == len(b) == len(aview)
 
-    print(a)
     a[4] = 132
     b[4] = 264
     assert b == aview
@@ -314,12 +544,24 @@ def test_MutateListView_get_set_del():
     assert len(a) == len(b) == len(aview)
 
 class ExcitedREListView(REListView, MutateListView):
+    def validate(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Doesn't validate ExcitedREListView")
+        return isinstance(value, str) and super().validate(value)
     def mutate(self, value):
         return super().mutate(value + '!')
     def demutate(self, value):
         if value[-1] != '!':
             raise ValueError("View must take values ending in '!'")
         return super().mutate(value[:-1])
+
+class NotAString:
+    def __bool__(self):
+        return True
+    def __add__(self, other):
+        return self
+    def __getitem__(self, key):
+        return 'g!'
 
 def test_ListView_multiple_inheritance():
     l = ["hello","gday","yo","sup","howzit"]
@@ -336,6 +578,9 @@ def test_ListView_multiple_inheritance():
         gyview[1] = ("hi")
     with pytest.raises(ValueError):
         gyview.append("yodelehihoo")
+    assert NotAString() + '!'
+    with pytest.raises(TypeError):
+        gyview.append(NotAString())
     gyview.append("yodelehihoo!")
     assert l == ["hello","gday","yo","yodelehihoo","sup","howzit"]
     assert hyview == ["hello!","yo!","yodelehihoo!","howzit!"]
@@ -354,3 +599,14 @@ def test_ListView_multiple_inheritance():
     assert gyview == ["gday!","yo!","yodelehihoo!"]
     assert l != hyview != gyview
 
+    assert hyview[0:2] == ["hello!","yo!"]
+    assert hyview[3:3] == []
+    hyview[3:3] = ["yaaaassss!"]
+    assert hyview == ["hello!", "yo!", "yodelehihoo!", "yaaaassss!","heya!","howzit!"]
+    possibilities = [
+        ["hello","gday","yo","yodelehihoo","sup","yaaaassss","heya","howzit"],
+        ["hello","gday","yo","yodelehihoo","yaaaassss","sup","heya","howzit"]
+    ]
+    assert l in possibilities
+    with pytest.raises(ValueError):
+        hyview[3:7] = ["hi"]
